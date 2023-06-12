87cd9f10f0bd14bb27b473e251202866d31fbac5
BlurryBat <53352897+BlurryBat@users.noreply.github.com>
2023-06-12 13:49:56 +0530

    update

--- june_12.js
+++ june_12.js
@@ -2,17 +2,17 @@ window.onload = function() {
 
     var blank = document.getElementById("blank");
     blank.addEventListener("keydown", function (e) {
-        if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        if (e.keyCode===13) {  //checks whether the pressed key is "Enter"
             validate(e);
         }
     });
 
     function validate(e) {
         var text = e.target.value;
-        if(text === "smile") {
+        if(text.toLowerCase() === "smile") {
             document.getElementById('blank').readOnly = true;
             document.getElementById("blank").style.border="1px solid green";
-            document.getElementById("img_here").src="https://raw.githubusercontent.com/BlurryBat/Cheese/0efa3e59e7579e7930a39442a07920d241eb3bec/smile.jpeg";
+            document.getElementById("img_here").src="/Volumes/workplace/test/smile.jpeg";
             startConfetti();
         } else {
             document.getElementById("blank").style.border="1px solid red";
